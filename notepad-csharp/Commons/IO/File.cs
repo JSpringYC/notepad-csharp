ae8b5430f4f8d0bb95f5fb85cf43b41f36e8ee45
佳木流泉 <JSpringYC@gmail.com>
2018-03-25 23:49:11 +0800

    锟斤拷目锟斤拷始锟斤拷

---
+++ notepad-csharp/Commons/IO/File.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Commons.IO
+{
+    /// <summary>
+    /// 文件对象，包含对文件操作的一系列方法
+    /// </summary>
+    public class Files
+    {
+        public static bool IsFile(string Path)
+        {
+            return File.Exists(Path);
+        }
+
+        public static bool IsFile(FileSystemInfo fileSystemInfo)
+        {
+            return IsFile(fileSystemInfo.FullName);
+        }
+
+        public static bool IsDirectory(string Path)
+        {
+            return File.Exists(Path);
+        }
+
+        public static bool IsDirectory(FileSystemInfo fileSystemInfo)
+        {
+            return IsDirectory(fileSystemInfo.FullName);
+        }
+
+        public static bool Exists(string Path)
+        {
+            FileStream stream = File.OpenRead(Path);
+            return File.Exists(Path) || Directory.Exists(Path);
+        }
+
+        public static bool Exists(FileSystemInfo fileSystemInfo)
+        {
+            return Exists(fileSystemInfo.FullName);
+        }
+    }
+}

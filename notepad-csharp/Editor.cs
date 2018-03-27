a1f0eceb59ee076c48ed9bbf8777ec83cf8fef7d
佳木流泉 <JSpringYC@gmail.com>
2018-03-28 00:49:35 +0800

    新增：文件打开、文件保存

---
+++ notepad-csharp/Editor.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace notepad_csharp
+{
+    class Editor
+    {
+        /// <summary>
+        /// 当前正在编辑的文件
+        /// </summary>
+        FileInfo ContentFile { get; set; }
+        /// <summary>
+        /// 显示文本的组件
+        /// </summary>
+        TextBox ContentText;
+        /// <summary>
+        /// 当前正在编辑的文件的内容
+        /// </summary>
+        string ContentFileText;
+
+        public Editor(TextBox textBox)
+        {
+            this.ContentText = textBox;
+        }
+
+        public bool IsTextChanged()
+        {
+            var Content = ContentText.Text;
+
+            if(String.IsNullOrEmpty(Content) && String.IsNullOrEmpty(ContentFileText))
+            {
+                return false;
+            }
+
+            return !Content.Equals(ContentFileText);
+        }
+
+        public bool Save()
+        {
+            if(ContentFile == null)
+            {
+                return false;
+            }
+            if (IsTextChanged())
+            {
+                var Content = ContentText.Text;
+                File.WriteAllText(ContentFile.FullName, Content);
+                ContentFileText = File.ReadAllText(ContentFile.FullName);
+                return Content.Equals(ContentFileText);
+            }
+            return true;
+        }
+
+        public bool Save(string path)
+        {
+            this.ContentFile = new FileInfo(path);
+
+            return Save();
+        }
+
+        public bool Open(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return false;
+            }
+            this.ContentFile = new FileInfo(path);
+            this.ContentFileText = File.ReadAllText(path);
+            this.ContentText.Text = ContentFileText;
+            return true;
+        }
+
+        public void Clear()
+        {
+            this.ContentFile = null;
+            this.ContentFileText = null;
+            this.ContentText.Text = "";
+        }
+
+        public string GetName()
+        {
+            if(ContentFile == null)
+            {
+                return "无标题";
+            }
+            else
+            {
+                return ContentFile.Name;
+            }
+        }
+    }
+}
--- notepad-csharp/MainWindow.xaml.cs
+++ notepad-csharp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,145 @@ namespace notepad_csharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Editor editor;
+
         public MainWindow()
         {
             InitializeComponent();
+            editor = new Editor(ContentText);
+        }
+
+        /// <summary>
+        /// 菜单：文件-新建
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewFileMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (SaveConfirm())
+            {
+                editor.Clear();
+                this.Title = "无标题 - 记事本";
+            }
+        }
+
+        /// <summary>
+        /// 菜单：文件-退出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (SaveConfirm())
+            {
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// 菜单：格式-自动换行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WrapMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (WrapMenu.IsChecked)
+            {
+                ContentText.TextWrapping = TextWrapping.WrapWithOverflow;
+            }
+            else
+            {
+                ContentText.TextWrapping = TextWrapping.NoWrap;
+            }
+        }
+
+        /// <summary>
+        /// 菜单：查看-状态栏
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StatusBarMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (StatusBarMenu.IsChecked)
+            {
+                StatusBarContainer.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBarContainer.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        /// <summary>
+        /// 保存确认
+        /// </summary>
+        /// <returns></returns>
+        private bool SaveConfirm()
+        {
+            if (!editor.Save())
+            {
+                MessageBoxResult result = MessageBox.Show("是否将更改保存到 " + editor.GetName() + "？", "记事本", MessageBoxButton.YesNoCancel);
+                if (result == MessageBoxResult.Cancel)
+                {
+                    return false;
+                }
+                if (result == MessageBoxResult.Yes)
+                {
+                    SaveFileDialog fileDialog = new SaveFileDialog();
+                    fileDialog.Title = "另存为";
+                    fileDialog.Filter = "文本文档(*.txt)|*.txt|所有文件(*.*)|*.*";
+                    fileDialog.FilterIndex = 1;
+                    fileDialog.RestoreDirectory = true;
+                    fileDialog.AddExtension = true;
+                    fileDialog.DefaultExt = ".txt";
+
+                    bool? save = fileDialog.ShowDialog();
+                    if(save.HasValue && save.Value)
+                    {
+                        var fileName = fileDialog.FileName;
+                        this.Title = new FileInfo(fileName).Name + " - 记事本";
+                        return editor.Save(fileName);
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 菜单：文件-打开
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (editor.IsTextChanged() && !SaveConfirm())
+            {
+                return;
+            }
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "打开";
+            fileDialog.Filter = "文本文档(*.txt)|*.txt|所有文件(*.*)|*.*";
+            fileDialog.FilterIndex = 1;
+            fileDialog.RestoreDirectory = true;
+            bool? open = fileDialog.ShowDialog();
+            if (open.HasValue && open.Value)
+            {
+                var fileName = fileDialog.FileName;
+                editor.Clear();
+                this.Title = new FileInfo(fileName).Name + " - 记事本";
+                editor.Open(fileName);
+            }
+        }
+
+        /// <summary>
+        /// 菜单：文件-保存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveMenu_Click(object sender, RoutedEventArgs e)
+        {
+            SaveConfirm();
         }
     }
 }
